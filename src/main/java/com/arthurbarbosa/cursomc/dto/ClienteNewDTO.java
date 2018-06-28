00132f02e2d6adf03d7bf271d75ac54e524a64cf
Arthur Barbosa <arthurbruno03@gmail.com>
2018-06-27 22:04:10 -0300

    Validação customizada de CPF ou CNPJ na inserção do cliente

--- src/main/java/com/arthurbarbosa/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/arthurbarbosa/cursomc/dto/ClienteNewDTO.java
@@ -2,29 +2,47 @@ package com.arthurbarbosa.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.arthurbarbosa.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres ")
 	private String nome;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Email(message = "Email inválido")
 	private String email;
+
+	@NotEmpty(message = "O preenchimento do cpf é obrigatório")
 	private String cpfOuCnpj;
+
 	private Integer tipo;
-	
+
 	private String logradouro;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String numero;
+
 	private String complemento;
 	private String bairro;
 	private String cep;
-	
+
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
-	
+
 	private Integer cidadeid;
-	
+
 	public ClienteNewDTO() {
-		
+
 	}
 
 	public String getNome() {
@@ -130,5 +148,5 @@ public class ClienteNewDTO implements Serializable {
 	public void setCidadeid(Integer cidadeid) {
 		this.cidadeid = cidadeid;
 	}
-	
+
 }
