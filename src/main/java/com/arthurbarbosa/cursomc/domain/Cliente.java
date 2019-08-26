a677df160eb0ff669a1f4c0b7eba55822862f4b7
Arthur Barbosa <arthurbruno03@gmail.com>
2019-08-25 21:16:32 -0400

    usando padrao de nome para imagens

--- src/main/java/com/arthurbarbosa/cursomc/domain/Cliente.java
+++ src/main/java/com/arthurbarbosa/cursomc/domain/Cliente.java
@@ -56,7 +56,6 @@ public class Cliente implements Serializable {
     @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
     private List<Pedido> pedidos = new ArrayList<>();
 
-    private String imageUrl;
 
     public Cliente() {
         addPerfil(Perfil.CLIENTE);
@@ -147,14 +146,6 @@ public class Cliente implements Serializable {
 
     public Set<Perfil> getPerfis() {
         return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
     }
 
     public void addPerfil(Perfil perfil) {
